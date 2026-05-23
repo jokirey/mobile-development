1ade39b07dc168e94c966177a8b80fd7d52f2a00
jokirey <jokirey.ja@gmail.com>
2026-05-23 19:40:34 +0300

    Урок 2 Задача 2 Тестирование обработки команд if else

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -20,19 +20,21 @@ void main(List<String> arguments) {
 
 void searchWikipedia(List<String>? arguments) {
   final String articleTitle;
-
   // If the user didn't pass in arguments, request an article title.
   if (arguments == null || arguments.isEmpty) {
     print('Please provide an article title.');
     // Await input and provide a default empty string if the input is null.
     articleTitle = stdin.readLineSync() ?? '';
   } else {
-    // Otherwise, join the arguments into a single string.
+    // Otherwise, join the arguments into the CLI into a single string
     articleTitle = arguments.join(' ');
   }
 
-  print('Current article title: $articleTitle');
+  print('Looking up articles about "$articleTitle". Please wait.');
+  print('Here ya go!');
+  print('(Pretend this is an article about "$articleTitle")');
 }
+
 
 
 void printUsage() { // Add this new function
