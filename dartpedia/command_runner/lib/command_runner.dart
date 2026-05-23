c746260330b0bf72ff5967bd4bacdcad4bc5987f
jokirey <jokirey.ja@gmail.com>
2026-05-23 21:15:47 +0300

    Урок 7 Задача 3: обновить пакет command_runner

--- dartpedia/command_runner/lib/command_runner.dart
+++ dartpedia/command_runner/lib/command_runner.dart
@@ -5,6 +5,10 @@ library;
 
 export 'src/arguments.dart';
 export 'src/command_runner_base.dart';
+export 'src/exceptions.dart';
 export 'src/help_command.dart';
-export 'src/exceptions.dart'; 
+export 'src/console.dart'; // Add this line
+
+// TODO: Export any libraries intended for clients of this package.
+
 // TODO: Export any libraries intended for clients of this package.
